28137f9ea791c81fd5ad3b315ec662b158dcd058
Ricardo Pereira <ricardo.pereira@buenonetworks.com.br>
2020-07-01 12:34:30 -0300

    Adicionada a regra de negocio procedural

---
+++ app.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+include_once 'procedural.php';
+
+echo "<br><br>";
+echo "////////////////////////////////////////////////////";
+echo "<br><br>";
+
+// include_once 'object.php';
---
+++ procedural.php
@@ -0,0 +1,58 @@
+<?php
+
+// --------------------------------------------------------------------------------
+// Forma procedural
+// --------------------------------------------------------------------------------
+
+function work(string $performer, string $period)
+{
+    $message = "Não sou ninguém! O que estou fazendo aqui?";
+
+    switch ($performer) {
+
+        case 'dancer':
+            $message = "Sou um dançarino\n"
+                . "Gostaria de programar, mas tenho coisas mais importantes para fazer!\n";
+
+            $message .= $period == 'day'
+                ? 'É dia: Estou dançando!'
+                : 'É noite: Não me amole... estou dormindo!';
+            break;
+
+        case 'developer':
+            $message = "Sou um candango que programa\n"
+                . "Não tenho vida... sou programador...\n";
+
+            $message .= $period == 'day'
+                ? 'É dia: Estou programando agora!'
+                : 'É noite: Eu nunca paro de programar!';
+            break;
+
+        case 'player':
+            $message = "Sou um jogador de LOL\n"
+                . "Não programo, pois meu é papel é jogar joguinho!\n";
+
+            $message .= $period == 'day'
+                ? 'É dia: Estou trabalhando agora!'
+                : 'É noite: Estou jogando agora!';
+            break;
+    }
+
+    return $message;
+}
+
+// Rotina
+echo "<pre>";
+echo work('dancer', 'day');
+echo "<br>----------------<br>";
+echo work('dancer', 'night');
+echo "<br><br>================<br><br>";
+echo work('developer', 'day');
+echo "<br>----------------<br>";
+echo work('developer', 'night');
+echo "<br><br>================<br><br>";
+echo work('player', 'day');
+echo "<br>----------------<br>";
+echo work('player', 'night');
+echo "<br>----------------<br>";
+echo "</pre>";
